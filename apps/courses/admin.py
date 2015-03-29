1da35c594e01b84e454245c1a9035d625d262691
Allen Joseph <allen.joseph.v.a@gmail.com>
2015-03-28 22:23:04 -0500

    update app

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Course, AcademyCourse
+from models import Course, AcademyCourse, AcademyCourseStudent
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
@@ -12,3 +12,6 @@ class AcademyCourseAdmin(admin.ModelAdmin):
     list_filter = ('course__department__faculty','course__department',)
 
 
+@admin.register(AcademyCourseStudent)
+class AcademyCourseStudentAdmin(admin.ModelAdmin):
+    list_display = ('id','student','academyCourse',)
